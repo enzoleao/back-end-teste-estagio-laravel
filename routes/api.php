254e516a82487d411505688175384d571d0c6765
Chrzddd <enzopinheiro6@gmail.com>
2023-05-18 23:52:06 -0300

    alterando rotas

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,7 @@ Use App\Companies;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CompaniesController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+
 
 Route::get('companies', [CompaniesController:: class, 'index']);
 Route::get('companies/{id}', [CompaniesController:: class, 'show']);
